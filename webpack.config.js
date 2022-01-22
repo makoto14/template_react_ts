5388ffd04e68bd046aad77290c4dbc967d272d5e
makoto14 <makoto.sasuke@gmail.com>
2022-01-22 14:34:49 +0900

    #5 revert fix webpack multiple page

--- webpack.config.js
+++ webpack.config.js
@@ -66,7 +66,8 @@ module.exports = {
   devServer: {
     static: {
       directory: path.resolve(__dirname, "./dist"),
-      publicPath: ["", "/**/*"],
+      // publicPath: ["", "/**/*"],
+      publicPath: ["/main", "/sub"],
       watch: true,
       serveIndex: true,
     },
